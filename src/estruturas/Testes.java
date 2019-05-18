11443663cd7fcb85296b062be26b219bfae3c4db
Rafael Borges <rafaelsfb@gmail.com>
2019-05-17 21:34:31 -0300

    Adicionando Telas e as Estruturas de dados

---
+++ src/estruturas/Testes.java
@@ -0,0 +1,24 @@
+package estruturas;
+
+import estruturas.arvore.Arvore;
+
+/**
+ *
+ * @author Rafael Borges
+ * github.com/RafaelB13
+ */
+public class Testes {
+    
+    public static void main(String[] args) {
+        
+        Pilha<Integer> p = new Pilha<>(10);
+        
+        for (int i = 0; i < 10; i++) {
+            p.inserir(i);
+        }
+        
+        
+        
+    }
+
+}
